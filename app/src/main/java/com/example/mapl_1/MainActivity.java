38baf259d2b14f7e944467365c34680f7e24daab
LeeGyXa <papkoud@gmail.com>
2022-09-11 17:35:29 +0300

    Commit 3

--- app/src/main/java/com/example/mapl_1/MainActivity.java
+++ app/src/main/java/com/example/mapl_1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mapl_1;
 
+import static android.graphics.Color.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -13,9 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @SuppressLint("SetTextI18n")
     public void onBtnClick (View view){
         TextView txtText = findViewById(R.id.textMessage);
         txtText.setText("New Message!");
+    }
+
+    public void colorChanger (View view){
+
+        TextView text = findViewById(R.id.textMessage);
+        text.setTextColor(Color.rgb(200,18,120));
     }
 
 }
