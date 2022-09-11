fe4630e0209b77ab4a842ad73eae17aed311271c
LeeGyXa <papkoud@gmail.com>
2022-09-11 16:16:05 +0300

    Commit 2

--- app/src/main/java/com/example/mapl_1/MainActivity.java
+++ app/src/main/java/com/example/mapl_1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mapl_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onBtnClick (View view){
+        TextView txtText = findViewById(R.id.textMessage);
+        txtText.setText("New Message!");
+    }
+
 }
